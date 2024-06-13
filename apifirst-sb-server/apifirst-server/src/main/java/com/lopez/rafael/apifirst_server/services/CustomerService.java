ae6f31c47480afab462a79985010234a0b4eda83
Rafael Lopez <lopez.rafael08@gmail.com>
2024-06-13 00:56:22 -0400

    Create Get Customer By Id endpoint

--- apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/services/CustomerService.java
+++ apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/services/CustomerService.java
@@ -3,7 +3,9 @@ package com.lopez.rafael.apifirst_server.services;
 import guru.springframework.apifirst.model.Customer;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<Customer> listCustomers();
+    Customer getCustomerById(UUID customerId);
 }
