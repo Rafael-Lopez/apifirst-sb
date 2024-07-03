abc54718d5cb35ff35b77b128415843a4063fdff
Rafael Lopez <lopez.rafael08@gmail.com>
2024-07-03 01:12:40 -0400

    Add create order api endpoint

--- apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/controllers/OrderController.java
+++ apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/controllers/OrderController.java
@@ -2,13 +2,14 @@ package com.lopez.rafael.apifirst_server.controllers;
 
 import com.lopez.rafael.apifirst_server.services.OrderService;
 import guru.springframework.apifirst.model.Order;
+import guru.springframework.apifirst.model.OrderCreate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,4 +33,15 @@ public class OrderController {
     public ResponseEntity<Order> getProductById(@PathVariable("orderId") UUID orderId) {
         return ResponseEntity.ok(orderService.getOrderById(orderId));
     }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewOrder(@RequestBody OrderCreate orderCreate){
+        Order savedOrder = orderService.saveNewOrder(orderCreate);
+
+        UriComponents uriComponents = UriComponentsBuilder.fromPath(BASE_URL + "/{orderId}")
+                .buildAndExpand(savedOrder.getId());
+
+        return ResponseEntity.created(URI.create(uriComponents.getPath())).build();
+    }
+
 }
--- apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/services/OrderService.java
+++ apifirst-sb-server/apifirst-server/src/main/java/com/lopez/rafael/apifirst_server/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.lopez.rafael.apifirst_server.services;
 
 import guru.springframework.apifirst.model.Order;
+import guru.springframework.apifirst.model.OrderCreate;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +10,5 @@ import java.util.UUID;
 public interface OrderService {
     List<Order> listOrders();
     Order getOrderById(UUID orderId);
+    Order saveNewOrder(OrderCreate orderCreate);
 }
